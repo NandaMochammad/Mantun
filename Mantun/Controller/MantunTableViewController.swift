d9729d1b0580424ed51edf83366f659ebef1294e
NandaMochammad <nanda.mocha@gmail.com>
2019-04-22 11:21:03 +0700

    Complete MVC COnversion

--- Mantun/Controller/MantunTableViewController.swift
+++ Mantun/Controller/MantunTableViewController.swift
@@ -10,15 +10,35 @@ import UIKit
 
 class MantunTableViewController: UITableViewController{
     
-    var itemArray = ["Get Breakfast", "Go to Bus Station", "Go to Academy", "FInish Udemy Module"]
-    var inits = 0
+    //Declare Model as Global Variable as! Array of item Object
+    var itemArray = [TitleModel]()
+    
+    //Declare User Defaults as Global Variable
     var defaults = UserDefaults.standard
 
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        if let items = defaults.array(forKey: "ArrayDoList") as? [String]{
+
+        //Input Data to Model
+        //1. Declare as a String
+        let titleModel = TitleModel()
+        //2. Set variable inside model with string
+        titleModel.titleString = "Get Breakfast"
+        //3. add the content of number 2 to array
+        itemArray.append(titleModel)
+        
+        let titleModel1 = TitleModel()
+        titleModel1.titleString = "Go to Bus Station "
+        itemArray.append(titleModel1)
+        
+        let titleModel2 = TitleModel()
+        titleModel2.titleString = "Go to Academy"
+        itemArray.append(titleModel2)
+        
+        //Get data Item Object from Model
+        if let items = defaults.array(forKey: "ArrayDoList") as? [TitleModel]{
             itemArray = items
         }
         
@@ -33,11 +53,18 @@ class MantunTableViewController: UITableViewController{
     
     //2. Define cellForRowAt to set the content on every cell
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let item = itemArray[indexPath.row]
+        
         //Declare the cell settings
         let cell = tableView.dequeueReusableCell(withIdentifier: "mantunCellTable", for: indexPath)
         
         //Set the cell's text from datassource
-        cell.textLabel?.text = itemArray[indexPath.row]
+        cell.textLabel?.text = item.titleString
+        
+        //Using Ternary Operation
+        // value = condition ? valueTrue : valueFalse
+        cell.accessoryType = item.done == true ? .checkmark : .none
         
         return cell
     }
@@ -50,12 +77,12 @@ class MantunTableViewController: UITableViewController{
         //Give an fade to white animation in selected row
         tableView.deselectRow(at: indexPath, animated: true)
         
-        //Give Checkmark in selected row
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        }else{
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        //Change value of selected row
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
+        //Reload Data to call Resource Data again
+        tableView.reloadData()
+
     }
     
     ////////////////////////////////////////////
@@ -63,16 +90,25 @@ class MantunTableViewController: UITableViewController{
     //MARK - Add New Item
     
     @IBAction func addDoList(_ sender: UIBarButtonItem) {
+        
         var newitem : UITextField!
         
         let alert = UIAlertController(title: "Add New Do List", message: " ", preferredStyle: .alert)
         
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            
             //Set what will happen when user tap the button
-            print("yeay")
-            self.itemArray.append(newitem.text ?? "")
-            self.tableView.reloadData()
+            let titleModel = TitleModel()
+            titleModel.titleString = newitem.text!
+            
+            //Add New DO list to array item object
+            self.itemArray.append(titleModel)
+            
+            //Save to userdefaults
             self.defaults.set(self.itemArray, forKey: "ArrayDoList")
+            
+            self.tableView.reloadData()
+
         }
         
         alert.addTextField { (textField) in
---
+++ Mantun/Model/TitleModel.swift
@@ -0,0 +1,14 @@
+//
+//  titleModel.swift
+//  Mantun
+//
+//  Created by Nanda Mochammad on 22/04/19.
+//  Copyright © 2019 nandamochammad. All rights reserved.
+//
+
+import Foundation
+
+class TitleModel{
+    var titleString : String = ""
+    var done : Bool = false
+}
